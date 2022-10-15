bc7ab23c0df6b8c267c43bedfa7a5642578eaa47
petrachoir <105375145+petrachoir@users.noreply.github.com>
2022-10-15 17:25:34 -0400

    added visual for selection, restart button

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ let roundWinner = "";
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
+const restartButton = document.getElementById("restart");
 const buttons = [rockButton, paperButton, scissorsButton];
 const playerScoreDOM = document.getElementById("player_score");
 const computerScoreDOM = document.getElementById("computer_score");
+const playerChoiceDOM = document.getElementById("player_choice");
+const computerChoiceDOM = document.getElementById("computer_choice");
 const logDOM = document.getElementById("log_message");
 const choices = ["rock", "paper", "scissors"];
+const choiceToEmoji = {
+  rock: "✊",
+  paper: "✋",
+  scissors: "✌",
+};
 
 // computer choice selection logic
 const getComputerChoice = () => {
@@ -50,21 +58,24 @@ const playRPS = (playerChoice) => {
 };
 
 const updateScore = (roundWinner, playerChoice, computerChoice) => {
+  playerChoiceDOM.textContent = choiceToEmoji[playerChoice];
+  computerChoiceDOM.textContent = choiceToEmoji[computerChoice];
+
   if (roundWinner === "Player") {
     playerScoreDOM.textContent++;
-    logDOM.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+    logDOM.textContent = `You win!\n ${playerChoice} beats ${computerChoice}.`;
   } else if (roundWinner === "Computer") {
     computerScoreDOM.textContent++;
-    logDOM.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
+    logDOM.textContent = `You lose!\n ${computerChoice} beats ${playerChoice}.`;
   } else {
-    logDOM.textContent = `It's a tie! You both selected ${playerChoice}.`;
+    logDOM.textContent = `It's a tie!\n You both selected ${playerChoice}.`;
   }
 
   if (playerScore === 3) {
-    return (logDOM.textContent = `You won the match! You beat the computer ${playerScore} to ${computerScore}!`);
+    return (logDOM.textContent = `You won the match!\n You beat the computer ${playerScore} to ${computerScore}!`);
   }
   if (computerScore === 3) {
-    return (logDOM.textContent = `You lost the match! The computer beat you ${computerScore} to ${playerScore}...`);
+    return (logDOM.textContent = `You lost the match!\n The computer beat you ${computerScore} to ${playerScore}...`);
   }
 };
 
@@ -77,6 +88,16 @@ const isGameOver = () => {
 rockButton.addEventListener("click", () => onClick("rock"));
 paperButton.addEventListener("click", () => onClick("paper"));
 scissorsButton.addEventListener("click", () => onClick("scissors"));
+restartButton.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  playerChoiceDOM.textContent = "";
+  computerChoiceDOM.textContent = "";
+  roundWinner = "";
+  playerScoreDOM.textContent = 0;
+  computerScoreDOM.textContent = 0;
+  logDOM.textContent = "Select a choice to begin";
+});
 
 // on click
 const onClick = (playerChoice) => {
